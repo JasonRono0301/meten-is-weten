498f0b50724246c207e6d3e0778f1092005c2b3a
Jason <99057964@mydavinci.nl>
2021-11-10 14:29:06 +0100

    else-statement

--- lab1/meten.py
+++ lab1/meten.py
@@ -1,10 +1,7 @@
 a = int(input('Type een getal in'))
 b = 5
 
-if a >= b:
-    print('Je kunt een groot zak patat kopen.')
-elif a >= 3:
-    min = a
-    print('Je kunt een middel zak patat kopen. '+ str(min))
+if a != b:
+    print('De twee getallen zijn niet gelijk')
 else:
-    print('Je hebt niet genoeg geld om een zak patat te kopen.')
+    print('De twee getallen zijn gelijk2')
