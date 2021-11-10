60b031535bb7b68cd0635e39d2f98e723cfa4246
Jason <99057964@mydavinci.nl>
2021-11-10 14:14:41 +0100

    input en if-statement

---
+++ lab1/meten.py
@@ -0,0 +1,8 @@
+a = int(input('Type een getal in'))
+b = int(input('Type een getal in'))
+
+if a > b:
+    max = a
+    print('a is het grootste getal:' + str(max))
+else:
+    print('a is niet groter dan b')
