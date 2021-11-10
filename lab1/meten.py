7f6a3b9293897d11525b0d959577ed766580a3a5
Jason <99057964@mydavinci.nl>
2021-11-10 14:34:08 +0100

    Min en Max

--- lab1/meten.py
+++ lab1/meten.py
@@ -1,7 +1,11 @@
-a = int(input('Type een getal in'))
-b = 5
+max = 10
+min = 0
 
-if a != b:
-    print('De twee getallen zijn niet gelijk')
+a = int(input('Type een getal in: '))
+
+if a == 10:
+    print('Het maximum is ' + str(max))
+elif a == 0:
+    print('Het minimum is ' + str(min))
 else:
-    print('De twee getallen zijn gelijk2')
+    print('Dit getal is niet het minimum of het maximum')
